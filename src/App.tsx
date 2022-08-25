7cf8b0f76d30a317a2c87dc49ceb1052ac921aa0
heny <cchuam@qq.com>
2022-08-25 14:28:28 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactSVG from '@/assets/react.svg'
+
+const App = () => {
+  return (
+    <div>
+      react svg
+      <img src={reactSVG} alt="" />
+      {/* <reactSVG /> */}
+    </div>
+  )
+}
+
+export default App
